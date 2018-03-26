1c676b1cb4a353418e4ea67766eb76aff2de5682
Tyler Chauhan <tjchauhan7@gmail.com>
2018-03-26 18:08:51 -0400

    Add update script

---
+++ playground/mongodb-update.js
@@ -0,0 +1,35 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+    if (err) {
+        return console.log('Unable to connect to MongoDB server.');
+    }
+    console.log('Connected to MongoDB server.');
+
+    // db.collection('Todos').findOneAndUpdate({
+    //     _id: new ObjectID('5ab7e56f86d5edbecb6c4559')
+    // }, {
+    //     $set : {
+    //         completed: true
+    //     }
+    // }, {
+    //     returnOriginal: false
+    // }).then((result) => {
+    //     console.log(result);
+    // });
+
+    db.collection('Users').findOneAndUpdate({
+        _id: new ObjectID('5aaad273bdb8b74e0b27988c')
+    }, {
+        $inc : {
+            age: 1
+        }
+    }, {
+        returnOriginal: false
+    }).then((result) => {
+        console.log(result);
+    });
+
+    // db.close();
+});
